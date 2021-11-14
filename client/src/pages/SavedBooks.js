0ca050fa4bdf7833575329786346e576a919a9f9
bowdenshot <anthonyelavezzo@gmail.com>
2021-11-13 19:05:08 -0800

    what could it be

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -19,12 +19,6 @@ const SavedBooks = () => {
   }
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
 
     try {
       await deleteBook({
